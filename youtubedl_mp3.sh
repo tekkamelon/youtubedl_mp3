effe26fad3407fc8b15c26fadacbe82dd5a702a2
tekkamelon <type2guntank@gmail.com>
2021-06-23 01:26:21 +0900

    DL済みと未DLを分別
    DL済みのurlを除外できるよう修正

--- youtubedl_mp3.sh
+++ youtubedl_mp3.sh
@@ -6,11 +6,13 @@ cd ~/Music
 # DLしたい動画のurlのリスト
 file=~/Documents/url.txt
 
-cat $file | while read url
+# DL済みのurlを除外
+cat $file | grep -v "Acquired " | while read url
 
 do
-	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url
-	sleep 2
+	# youtube-dlでmp3をDL,urlリストの行頭に"Acquired "を追記,sedの区切り文字に" | "を使用
+	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url ; sed -i "s|$(cat $file | grep $url)|Acquired $(cat $file | grep $url)|g" $file
+	sleep 3
 done
 
 exit 0
