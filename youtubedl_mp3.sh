c68f2b05a8352558196f4103b52f136f8b63f448
tekkamelon <type2guntank@gmail.com>
2021-06-24 02:21:33 +0900

    軽微な修正
    url以外の文字列の除外,DLに失敗したurlに"Acquired "を挿入しないよう修正

--- youtubedl_mp3.sh
+++ youtubedl_mp3.sh
@@ -6,12 +6,12 @@ cd ~/Music
 # DLしたい動画のurlのリスト
 file=~/Documents/url.txt
 
-# DL済みのurlを除外
-cat $file | grep -v "Acquired " | while read url
+# DL済みのurl,url以外の文字列を除外
+cat $file | grep -v "Acquired " | grep "http" | while read url
 
 do
 	# youtube-dlでmp3をDL,urlリストの行頭に"Acquired "を追記,sedの区切り文字に" | "を使用
-	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url ; sed -i "s|$(cat $file | grep $url)|Acquired $(cat $file | grep $url)|g" $file
+	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url && sed -i "s|$(cat $file | grep $url)|Acquired $(cat $file | grep $url)|g" $file
 	sleep 3
 done
 
