4f95cca48e3a489c4699c06c904b891ec3d5bed5
tekkamelon <type2guntank@gmail.com>
2021-06-24 03:13:37 +0900

    軽微な修正
    sedの引数を修正

--- youtubedl_mp3.sh
+++ youtubedl_mp3.sh
@@ -10,9 +10,10 @@ file=~/Documents/url.txt
 cat $file | grep -v "Acquired " | grep "http" | while read url
 
 do
+	
 	# youtube-dlでmp3をDL,urlリストの行頭に"Acquired "を追記,sedの区切り文字に" | "を使用
-	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url && sed -i "s|$(cat $file | grep $url)|Acquired $(cat $file | grep $url)|g" $file
-	sleep 3
+	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url && sed -i "s|$url|Acquired $url|g" $file ; sleep 3
+
 done
 
 exit 0
