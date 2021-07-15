63f7b62e7934f02d3cd8bf00368a50dbb848576a
tekkamelon <type2guntank@gmail.com>
2021-07-16 00:36:30 +0900

    引数を受け取るよう修正
    保存先のディレクトリ,URLのリストのディレクトリの絶対パスを引数として受け取る

--- youtubedl_mp3.sh
+++ youtubedl_mp3.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-# 保存先のディレクトリ
-cd ~/Music
-
-# DLしたい動画のurlのリスト
-file=~/Documents/url.txt
+保存先のディレクトリ
+cd $2
 
 # DL済みのurl,url以外の文字列を除外
-cat $file | grep -v "Acquired " | grep "http" | while read url
+cat $1 | grep -v "Acquired" | grep "http" | while read url 
 
 do
 	
 	# youtube-dlでmp3をDL,urlリストの行頭に"Acquired "を追記,sedの区切り文字に" | "を使用
-	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url && sed -i "s|$url|Acquired $url|g" $file ; sleep 3
+	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url && sed -i "s|$url|Acquired $url|g" $1 ; sleep 3
 
 done
 
