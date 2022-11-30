01440972ec3288553bcd3e51b9a18db075f85417
tekkamelon <type2guntank@gmail.com>
2022-12-01 00:09:29 +0900

    軽微な修正
    コメント及びインデントの修正

--- youtubedl_mp3.sh
+++ youtubedl_mp3.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh -eu
 
-保存先のディレクトリ
+# 保存先のディレクトリ
 cd $2
 
 # DL済みのurl,url以外の文字列を除外
@@ -8,8 +8,11 @@ cat $1 | grep -v "Acquired" | grep "http" | while read url
 
 do
 	
-	# youtube-dlでmp3をDL,urlリストの行頭に"Acquired "を追記,sedの区切り文字に" | "を使用
-	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url && sed -i "s|$url|Acquired $url|g" $1 ; sleep 3
+	# youtube-dlでmp3をDL,urlリストの行頭に"Acquired "を追記
+	youtube-dl -x --audio-format mp3 -o "%(title)s.%(ext)s" $url &&
+
+	# 区切り文字に" | "を使用,読み込んだurlの行頭に"Acquired "を追加
+	sed -i "s|$url|Acquired $url|g" $1 ; sleep 3
 
 done
 
